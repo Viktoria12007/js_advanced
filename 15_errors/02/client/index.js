058a3d76fa89ec601e53bf7f33e06d0a96ada71b
Vika <viki12007@inbox.ru>
2021-10-01 20:26:52 +0300

    first check of the second task

--- 15_errors/02/client/index.js
+++ 15_errors/02/client/index.js
@@ -1,11 +1,28 @@
 async function getData(way) {
-    const response = await fetch(way);
+    let response = await fetch(way);
+    console.log(response);
+    if (response.status === 404) {
+        throw new Error('Список товаров пуст');
+    }
+    if (response.status === 500) {
+        response = await fetch(way);
+        if (response.status === 500) {
+            response = await fetch(way);
+            if (response.status === 500) {
+                throw new Error('Произошла ошибка, попробуйте обновить страницу позже');
+            }
+        }
+       
+    }
+
     const data = response.json();
+    console.log(data);
     return data;
 }
-async function createCatalog(dataProducts) {
+
+function createCatalog(dataProducts) {
     const catalog = document.getElementById('catalog');
-    // const dataProducts = await getProductsData();
+    const fragment = document.createDocumentFragment();
     dataProducts.products.forEach(element => {
         const card = document.createElement('div');
         card.classList.add('card', 'mb-5');
@@ -16,17 +33,64 @@ async function createCatalog(dataProducts) {
           <p class="card-text">${element.price} $</p>
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>`
-        catalog.append(card);
-        // const info = createInfo();
-        // catalog.append(info);
+        fragment.appendChild(card);
     });
+    catalog.append(fragment);
 }
-// function createInfo() {
-//     const infoElement = document.createElement('div');
-//     infoElement.classList.add('position-absolute', 'bottom-0', 'end-0');
-//     infoElement.textContent = 'Hello!';
-//     return infoElement;
-// }
 
-const dataProducts = await getData('/api/products?status=200');
-createCatalog(dataProducts);
+function hideSpinner(action, data) {
+    const spinner = document.getElementById('spinner');
+    spinner.classList.add('loaded_hiding');
+     window.setTimeout(() => {
+         spinner.classList.add('loaded');
+         spinner.classList.remove('loaded_hiding');
+         if(action === 'create') {
+         createCatalog(data);
+         }
+     }, 500);
+ }
+
+//  function hideSpinner() {
+//     const spinner = document.getElementById('spinner');
+//     spinner.classList.add('loaded_hiding');
+//      window.setTimeout(() => {
+//          spinner.classList.add('loaded');
+//          spinner.classList.remove('loaded_hiding');
+//      }, 500);
+//  }
+
+const info = document.getElementById('info');
+const infoBody = document.querySelector('.toast-body');
+
+try {
+    const dataProducts = await getData('/api/products');
+    hideSpinner('create', dataProducts);
+}
+catch(error) {
+    if (error.message === 'Unexpected end of JSON input') {
+        infoBody.textContent = 'Произошла ошибка, попробуйте обновить страницу позже'
+    }
+    else {
+        infoBody.textContent = error.message;
+    }
+  info.classList.add('show');
+  window.setTimeout(() => {
+     info.classList.add('loaded_hiding');
+  }, 3000)
+  window.setTimeout(() => {
+     info.classList.remove('show', 'loaded_hiding');
+  }, 4000)
+}
+finally {
+    hideSpinner('don`t create');
+}
+
+// getData('/api/products?status=200').then((resolve)=> {
+//     hideSpinner('create', resolve);
+//     // createCatalog(resolve);
+// }).catch(error => {
+//     infoBody.textContent = 'Произошла ошибка, попробуйте обновить страницу позже';
+//     info.classList.add('show');
+// }).finally(() => {
+//     hideSpinner('don`t create');
+// })
