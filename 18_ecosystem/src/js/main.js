4a4b77b4c373cd6d78701a840901e84e14db08c9
Виктория Голубова <v.golubova@denexy.com>
2023-06-14 18:20:12 +0400

    Add form validation logic.

--- 18_ecosystem/src/js/main.js
+++ 18_ecosystem/src/js/main.js
@@ -1,28 +1,131 @@
-import { el, setChildren } from 'redom';
+import { setAttr } from 'redom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import payform from 'payform';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import isEmail from 'validator/lib/isEmail';
+import urls from './urls';
+import dom from './dom';
 
-const container = el('div', { className: 'container vh-100 d-flex flex-column align-items-center justify-content-center' });
-const card = el('div', { className: 'card w-50' });
-const cardHeader = el('h3', { textContent: 'Оплата банковской картой', className: 'card-header text-center' });
-const cardBody = el('div', { className: 'card-body p-3' });
-const form = el('form', { className: 'd-flex flex-column' });
-const numberCard = el('label', { textContent: 'Номер банковской карты:', className: 'form-label d-flex flex-column mb-2' }, el('input', {
-  className: 'form-control mt-2', type: 'text', autofocus: true, required: true, placeholder: 'Номер банковской карты',
-}));
-const expirationDateCard = el('label', { textContent: 'Дата окончания действия карты (ММ/ГГ):', className: 'form-label d-flex flex-column mb-2' }, el('input', {
-  className: 'form-control mt-2', type: 'text', required: true, placeholder: 'Дата окончания действия карты',
-}));
-const cvcCVV = el('label', { textContent: 'CVC/CVV (3 цифры на обороте карты):', className: 'form-label d-flex flex-column mb-2' }, el('input', {
-  className: 'form-control mt-2', type: 'text', required: true, placeholder: 'CVC/CVV',
-}));
-const email = el('label', { textContent: 'Email для отправки онлайн-чека:', className: 'form-label d-flex flex-column mb-3' }, el('input', {
-  className: 'form-control mt-2', type: 'email', required: true, placeholder: 'Email',
-}));
-const button = el('button', {
-  textContent: 'Оплатить', className: 'btn btn-secondary', type: 'submit', disabled: true, style: { 'margin-left': 'auto' },
+payform.cardNumberInput(dom.cardNumber);
+payform.expiryInput(dom.cardExpirationDate);
+payform.cvcInput(dom.cvcCVV);
+
+function setStyle(e, state, input, feedback) {
+  if (state) {
+    if (e.target.name === 'cardNumber') {
+      const paymentSystem = payform.parseCardType(e.target.value);
+      setAttr(dom.brandLogo, {
+        src: `${urls[paymentSystem]}`,
+        alt: `${paymentSystem}`,
+        className: 'logo mb-2 visible',
+      });
+    }
+    setAttr(input, {
+      className: 'form-control is-valid',
+    });
+    setAttr(feedback, {
+      textContent: 'Успешно!',
+      className: 'valid-feedback',
+    });
+  } else {
+    if (e.target.name === 'cardNumber') {
+      setAttr(dom.brandLogo, {
+        src: '',
+        alt: '',
+        className: 'logo mb-2',
+      });
+    }
+    setAttr(input, {
+      className: 'form-control is-invalid',
+    });
+    if (!e.target.value) {
+      setAttr(feedback, {
+        textContent: 'Поле обязятельно к заполнению!',
+      });
+    } else {
+      setAttr(feedback, {
+        textContent: 'Не верный формат!',
+      });
+    }
+    setAttr(feedback, {
+      className: 'invalid-feedback',
+    });
+  }
+}
+
+function cancelStyle(e, input, feedback) {
+  if (e.target.name === 'cardNumber') {
+    setAttr(dom.brandLogo, {
+      src: '',
+      alt: '',
+      className: 'logo mb-2',
+    });
+  }
+  setAttr(input, {
+    className: 'form-control',
+  });
+  setAttr(feedback, {
+    textContent: 'Не верный формат!',
+    className: 'invalid-feedback',
+  });
+}
+
+const statusValidation = {
+  cardNumber: null,
+  cardExpirationDate: null,
+  cvcCVV: null,
+  email: null,
+};
+
+function checkReadyForm() {
+  if (Object.values(statusValidation).every((element) => element)) {
+    setAttr(dom.button, {
+      disabled: false,
+    });
+  } else {
+    setAttr(dom.button, {
+      disabled: true,
+    });
+  }
+}
+
+dom.cardNumber.addEventListener('blur', (e) => {
+  statusValidation.cardNumber = payform.validateCardNumber(e.target.value);
+  setStyle(e, statusValidation.cardNumber, dom.cardNumber,
+    dom.cardNumberFeedback);
+  checkReadyForm();
+});
+dom.cardExpirationDate.addEventListener('blur', (e) => {
+  const { month, year } = payform.parseCardExpiry(e.target.value);
+  statusValidation.cardExpirationDate = payform.validateCardExpiry(month, year);
+  setStyle(e, statusValidation.cardExpirationDate, dom.cardExpirationDate,
+    dom.cardExpirationDateFeedback);
+  checkReadyForm();
+});
+dom.cvcCVV.addEventListener('blur', (e) => {
+  statusValidation.cvcCVV = payform.validateCardCVC(e.target.value);
+  setStyle(e, statusValidation.cvcCVV, dom.cvcCVV, dom.cvcCVVFeedback);
+  checkReadyForm();
+});
+dom.email.addEventListener('blur', (e) => {
+  statusValidation.email = isEmail(e.target.value);
+  setStyle(e, statusValidation.email, dom.email, dom.emailFeedback);
+  checkReadyForm();
 });
 
-setChildren(document.body, [container]);
-setChildren(container, [card]);
-setChildren(card, [cardHeader, cardBody]);
-setChildren(cardBody, [form]);
-setChildren(form, [numberCard, expirationDateCard, cvcCVV, email, button]);
+dom.cardNumber.addEventListener('input', (e) => {
+  cancelStyle(e, dom.cardNumber, dom.cardNumberFeedback);
+});
+dom.cardExpirationDate.addEventListener('input', (e) => {
+  cancelStyle(e, dom.cardExpirationDate, dom.cardExpirationDateFeedback);
+});
+dom.cvcCVV.addEventListener('input', (e) => {
+  cancelStyle(e, dom.cvcCVV, dom.cvcCVVFeedback);
+});
+dom.email.addEventListener('input', (e) => {
+  cancelStyle(e, dom.email, dom.emailFeedback);
+});
+
+dom.button.addEventListener('click', (e) => {
+  e.preventDefault();
+});
