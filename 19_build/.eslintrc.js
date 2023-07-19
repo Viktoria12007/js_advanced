d8686473ee6b13d72d436f6adf75df54c3d2f5b0
Виктория Голубова <v.golubova@denexy.com>
2023-07-19 12:22:58 +0400

    Fixed padding and quotes

--- 19_build/.eslintrc.js
+++ 19_build/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   root: true,
   extends: ['airbnb-base'],
-    parserOptions: {
-    ecmaVersion: "latest"
+  parserOptions: {
+    ecmaVersion: 'latest',
   },
   env: {
     browser: true,
